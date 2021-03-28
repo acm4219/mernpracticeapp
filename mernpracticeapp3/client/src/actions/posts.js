b68c3c397e1d2b139cd1d9c51e4bdd09417fe837
Alex McDonald <acm4219@gmail.com>
2021-03-28 00:11:29 -0500

    did a lot of framework for the base model, got the posts to work and such

---
+++ mernpracticeapp3/client/src/actions/posts.js
@@ -0,0 +1,32 @@
+import * as api from "../api";
+
+//Action Creators 
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch(err) {
+        console.log(err.message);
+    }    
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try{
+        const { data } = await api.createPost(post);
+
+        dispatch({type: 'CREATE', payload: data})
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatedPost(id, post);
+
+        dispatch({type: 'UPDATE', payload: data})
+    } catch(err) {
+        console.log(err.message);
+    }
+}
--- mernpracticeapp3/server/controllers/posts.js
+++ mernpracticeapp3/server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
@@ -12,6 +13,27 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send("post creation");
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch(err) {
+        res.status(409).json({ message: err.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("no post with that id");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
 }
