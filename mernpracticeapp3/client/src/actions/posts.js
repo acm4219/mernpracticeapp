0871e5c3d5ee854252ce29358baae3e39c3dfb14
Alex McDonald <acm4219@gmail.com>
2021-03-28 15:19:22 -0500

    got the basic functionality down just need to adapt it for my needs

--- mernpracticeapp3/client/src/actions/posts.js
+++ mernpracticeapp3/client/src/actions/posts.js
@@ -27,6 +27,28 @@ export const updatePost = (id, post) => async (dispatch) => {
 
         dispatch({type: 'UPDATE', payload: data})
     } catch(err) {
-        console.log(err.message);
+        console.log(err);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: 'DELETE', payload: id})
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({type: 'UPDATE', payload: data});
+
+
+    } catch(err) {
+        console.log(err);
     }
 }
--- mernpracticeapp3/client/src/components/Form/Form.js
+++ mernpracticeapp3/client/src/components/Form/Form.js
@@ -28,19 +28,26 @@ function Form({ currentId, setCurrentId }) {
 
         if(currentId) {
             dispatch(updatePost(currentId, postData))
+            
         } else {
             dispatch(createPost(postData))
         }
-
-        dispatch(createPost(postData))
+        clear();
     }
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({
+            creator: '', 
+            title: '', 
+            message: '', 
+            tags: '', 
+            selectedFile: ''
+        });
     }
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
+                <Typography variant="h6">{currentId ? "Editing" : "Creating"} a Memory</Typography>
                 <TextField 
                     name="creator" 
                     variant="outlined" 
